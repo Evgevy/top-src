36f1b7636c5c65549a8467e0de5f00d821f8f7e2
unknown <djek.uait1998@gmail.com>
2025-02-10 07:00:53 +0300

    first commit

---
+++ source/js/modules/init-menu.js
@@ -0,0 +1,17 @@
+const initMenu = () => {
+    // const toggleButton = document.querySelector('[data-toggle-menu]');
+    // const menu = document.querySelector('.main-nav__menu');
+
+    // toggleButton.addEventListener('click', function() {
+    //     const isExpanded = toggleButton.getAttribute('aria-pressed') === 'true';
+        
+    //     // Переключаем состояние кнопки
+    //     toggleButton.setAttribute('aria-pressed', !isExpanded);
+        
+    //     // Выезжаем или скрываем меню
+    //     menu.classList.toggle('is-visible', !isExpanded);
+    // });
+
+}
+
+export{initMenu};
